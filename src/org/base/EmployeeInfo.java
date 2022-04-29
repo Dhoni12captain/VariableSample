ef4f2202fdbd6c30f4ac5ad0db8a97708257a762
Dhoni12captain <sanchintu1212@gmail.com>
2022-04-29 15:37:46 +0530

    mad works

--- src/org/base/EmployeeInfo.java
+++ src/org/base/EmployeeInfo.java
@@ -2,6 +2,19 @@ package org.base;
 
 public class EmployeeInfo {
 	static int a = 10;
+	private void add() {
+		a=100;
+		
+
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(a);
+		EmployeeInfo ee = new EmployeeInfo();
+		ee.add();
+		System.out.println(a);
+		
+	}
 	
 		
 	}
