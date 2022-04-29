722b3add3ab782152e8a75fda230bb1de3f2c908
Dhoni12captain <sanchintu1212@gmail.com>
2022-04-29 16:14:20 +0530

    rak works

--- src/org/base/EmployeeInfo.java
+++ src/org/base/EmployeeInfo.java
@@ -1,22 +1,10 @@
 package org.base;
 
 public class EmployeeInfo {
-	static int a = 10;
-	int c;
-	private void add(int a,int b) {
-	a=15;
-	b=20;
-	c = a+b;
-
+	static int a = 20;
+	 public static void main(String[] args) {
+		System.out.println(a);
 	}
-	public static void main(String[] args) {
-		System.out.println(a);	
-		EmployeeInfo e = new EmployeeInfo();
-		e.add(100, 200);
-		System.out.println(e.c);
-		
-	}
-	
 		
 	}
 
